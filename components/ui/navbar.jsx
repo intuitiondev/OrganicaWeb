77ca163071cf84e4b5074b5eb44e764598309147
Cin-1 <cinthiapardos@gmail.com>
2021-04-06 11:46:16 -0300

    cajon

--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import Logo from './logo';
-import Menu from './menu';
+import React from "react";
+import Logo from "./logo";
+import Menu from "./menu";
+import Box from "@material-ui/core/Box";
 
-import {
-    Box
-} from '@material-ui/core';
+const Navbar = (props) => {
+  return (
+    <Box
+      pl={3}
+      pr={3}
+      style={{
+        backgroundColor: "#fff",
+        borderBottom: "solid 1px #E3E4E5",
+        position: "fixed",
+        width: "100%",
+        zIndex: "99995",
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Logo />
+        <Menu />
+      </Box>
 
-const Navbar = props => {
-    return ( 
-        <Box pl={3} pr={3} 
-            style={{
-                backgroundColor: "#fff",
-                borderBottom: "solid 1px #E3E4E5",
-                position: "fixed",
-                width: "100%",
-                zIndex: "99995"
-            }}
-        >
-                <Box display="flex"flexDirection="row" justifyContent="space-between" alignItems="center">
-                    <Logo />
-                    <Menu />
-                </Box>
+      <main>{props.children}</main>
+    </Box>
+  );
+};
 
-            <main>
-                {props.children}
-            </main>
-        </Box>
-     );
-}
- 
 export default Navbar;
