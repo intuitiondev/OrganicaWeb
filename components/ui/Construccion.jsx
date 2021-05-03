5f80748e534e011883df624975756838a5183729
Cin-1 <cinthiapardos@gmail.com>
2021-05-03 15:47:45 -0300

    imagen

---
+++ components/ui/Construccion.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Suenios from "../../public/images/suenios.jpg";
+
+const Imagen = () => {
+  return <img style={{ width: "100%" }} src={Suenios}></img>;
+};
+
+export default Imagen;
--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,14 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../components/ui/theme";
+import Imagen from "../components/ui/Construccion";
 
 export default function Home() {
   const router = useRouter();
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LangSelector />
-        <p>
-          <Link href="/ejemplo" locale={router.locale}>
-            Ir a ejemplo
-          </Link>
-        </p>
+        <Imagen />
       </ThemeProvider>
     </>
   );
